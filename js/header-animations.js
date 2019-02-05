d55f52951bd10cd0970ea5d4250845a89d812074
clem9281 <clem9281@gmail.com>
2019-02-05 16:26:42 -0700

    added written content and changed the underline of the repeating header

---
+++ js/header-animations.js
@@ -0,0 +1,21 @@
+let logo = document.querySelector("nav .logo");
+let bars = document.querySelector("nav .menu-button");
+let headerContent = document.querySelector(".header-content");
+
+let timeIncrement = 0;
+console.log(boxes);
+function headerContentFadeIn() {
+  TweenMax.fromTo(
+    headerContent,
+    0.8,
+    { opacity: 0, left: "-1rem" },
+    { opacity: 1, left: 0 }
+  );
+}
+function navItemsFadeIn() {
+  TweenMax.fromTo(logo, 0.8, { opacity: 0 }, { opacity: 1, delay: 0.8 });
+  TweenMax.fromTo(bars, 0.8, { opacity: 0 }, { opacity: 1, delay: 0.8 });
+}
+
+headerContentFadeIn();
+navItemsFadeIn();
