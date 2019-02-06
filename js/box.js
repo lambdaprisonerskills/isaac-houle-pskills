56eb97857e9bf543c9e6659fc399173d43f284cb
clem9281 <clem9281@gmail.com>
2019-02-06 14:33:01 -0700

    standardized font sizes across page

--- js/box.js
+++ js/box.js
@@ -1,7 +1,11 @@
 class Box {
   constructor(element) {
     this.element = element;
+    // get the element that holds the background color
     this.background = this.element.querySelector(".bg-color");
+    // also get all the links
+    this.links = this.element.querySelectorAll("a");
+    // listen for mouseover
     this.element.addEventListener("mouseover", () => {
       this.mouseOver();
     });
@@ -10,11 +14,15 @@ class Box {
     });
   }
   mouseOver() {
+    // change the background color to the banner background
     this.background.style.background = "rgba(36, 41, 67, 0.9)";
+    // I wanted it to be more obvious that the links were clickable without changing the current styles or layout, so on hover of the box element the links change to their hover color
+    this.links.forEach(link => (link.style.color = "#6fc3df"));
   }
   mouseOff() {
     // by just removing the inline style, the element reverts to how it is styled in less
     this.background.style.background = "";
+    this.links.forEach(link => (link.style.color = ""));
   }
 }
 
